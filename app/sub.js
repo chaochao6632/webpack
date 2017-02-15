af464f54800e489e3fa884a5044f2180c7f72b85
guokangf <782216385@qq.com>
2017-02-15 17:22:56 +0800

    first

---
+++ app/sub.js
@@ -0,0 +1,13 @@
+/*common js 风格*/
+// function generateText() {
+//     var element = document.createElement('h2');
+//     element.innerHTML = "Hello h2 world";
+//     return element;
+// }
+// module.exports = generateText;
+/*es6 风格*/
+export default function () {
+    var element = document.createElement('h2');
+    element.innerHTML = "Hello h2 world hahaha";
+    return element;
+}
